04d4b4e54ec4e13cd254ba11d5d0334d42b5ec02
zerogoon <kckzero@gmail.com>
2021-02-06 01:08:51 +0900

    dialog_1

---
+++ dialog/point.js
@@ -0,0 +1,39 @@
+export class Point {
+    constructor(x, y) {
+        this.x = x || 0;
+        this.y = y || 0;
+    }
+
+    add(point) {
+        this.x += point.x;
+        this.y += point.y;
+        return this;
+    }
+
+    substract(point) {
+        this.x -= point.x;
+        this.y -= point.y;
+        return this;
+    }
+
+    reduce(value) {
+        this.x *= value.x;
+        this.y *= value.y;
+        return this;
+    }
+
+    collide(point, width, height) {
+        if (this.x >= point.x &&
+            this.x <= point.x + width &&
+            this.y >= point.y &&
+            this.y <= point.y + height) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    clone() {
+        return new Point(this.x, this.y);
+    }
+}

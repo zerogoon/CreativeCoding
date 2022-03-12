c265e84e9adf0dc6bf71f5d1f4c66877cbbe176d
zerogoon <kckzero@gmail.com>
2022-03-12 15:47:45 +0900

    자바스크립트 컬러 추출 물결효과 완료

--- canvas/app.js
+++ canvas/app.js
@@ -1,111 +1,142 @@
 import { Ripple } from "./ripple.js";
+import { Dot } from "./dot.js";
+import { collide } from "./utils.js";
 
 class App {
-  constructor() {
-    this.canvas = document.createElement("canvas");
-    document.body.appendChild(this.canvas);
-    this.ctx = this.canvas.getContext("2d");
+    constructor() {
+        this.canvas = document.createElement("canvas");
+        document.body.appendChild(this.canvas);
+        this.ctx = this.canvas.getContext("2d");
 
-    this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
+        this.tmpCanvas = document.createElement("canvas");
+        this.tmpCtx = this.tmpCanvas.getContext("2d");
 
-    this.ripple = new Ripple();
+        this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
 
-    window.addEventListener("resize", this.resize.bind(this), false);
-    this.resize();
+        this.ripple = new Ripple();
 
-    this.isLoaded = false;
-    this.imgPos = {
-      x: 0,
-      y: 0,
-      width: 0,
-      height: 0,
-    };
+        window.addEventListener("resize", this.resize.bind(this), false);
+        this.resize();
 
-    this.image = new Image();
-    this.image.src = "gogh1.jpg";
-    this.image.onload = () => {
-      this.isLoaded = true;
-      this.drawImage();
-    };
+        this.radius = 10;
+        this.pixelSize = 30;
+        this.dots = [];
 
-    window.requestAnimationFrame(this.animate.bind(this));
+        this.isLoaded = false;
+        this.imgPos = {
+            x: 0,
+            y: 0,
+            width: 0,
+            height: 0,
+        };
 
-    this.canvas.addEventListener("click", this.onClick.bind(this), false);
-  }
+        this.image = new Image();
+        this.image.src = "gogh2.jpg";
+        this.image.onload = () => {
+            this.isLoaded = true;
+            this.drawImage();
+        };
 
-  resize() {
-    this.stageWidth = document.body.clientWidth;
-    this.stageHeight = document.body.clientHeight;
+        window.requestAnimationFrame(this.animate.bind(this));
 
-    this.canvas.width = this.stageWidth * this.pixelRatio;
-    this.canvas.height = this.stageHeight * this.pixelRatio;
-    this.ctx.scale(this.pixelRatio, this.pixelRatio);
-
-    this.ripple.resize(this.stageWidth, this.stageHeight);
-
-    if (this.isLoaded) {
-      this.drawImage();
-    }
-  }
-
-  drawImage() {
-    const stageRatio = this.stageWidth / this.stageHeight;
-    const imgRatio = this.image.width / this.image.height;
-
-    this.imgPos.width = this.stageWidth;
-    this.imgPos.height = this.stageHeight;
-
-    if (imgRatio > stageRatio) {
-      this.imgPos.width = Math.round(
-        this.image.width * (this.stageHeight / this.image.height)
-      );
-      this.imgPos.x = Math.round((this.stageWidth - this.imgPos.width) / 2);
-    } else {
-      this.imgPos.height = Math.round(
-        this.image.height * (this.stageWidth / this.image.width)
-      );
-      this.imgPos.y = Math.round((this.stageHeight - this.imgPos.height) / 2);
+        this.canvas.addEventListener("click", this.onClick.bind(this), false);
     }
 
-    this.ctx.drawImage(
-      this.image,
-      0,
-      0,
-      this.image.width,
-      this.image.height,
-      this.imgPos.x,
-      this.imgPos.y,
-      this.imgPos.width,
-      this.imgPos.height
-    );
-  }
+    resize() {
+        this.stageWidth = document.body.clientWidth;
+        this.stageHeight = document.body.clientHeight;
 
-  animate() {
-    window.requestAnimationFrame(this.animate.bind(this));
+        this.canvas.width = this.stageWidth * this.pixelRatio;
+        this.canvas.height = this.stageHeight * this.pixelRatio;
+        this.ctx.scale(this.pixelRatio, this.pixelRatio);
 
-    this.ripple.animate();
-  }
+        this.tmpCanvas.width = this.stageWidth;
+        this.tmpCanvas.height = this.stageHeight;
 
-  onclick(e) {
-    this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+        this.ripple.resize(this.stageWidth, this.stageHeight);
 
-    this.ctx.drawImage(
-      this.image,
-      0,
-      0,
-      this.image,
-      width,
-      this.image.height,
-      this.imgPos.x,
-      this.imgPos.y,
-      this.imgPos.width,
-      this.imgPos.height
-    );
+        if (this.isLoaded) {
+            this.drawImage();
+        }
+    }
 
-    this.ripple.start(e.offsetX, e.offsetY);
-  }
+    drawImage() {
+        const stageRatio = this.stageWidth / this.stageHeight;
+        const imgRatio = this.image.width / this.image.height;
+
+        this.imgPos.width = this.stageWidth;
+        this.imgPos.height = this.stageHeight;
+
+        if (imgRatio > stageRatio) {
+            this.imgPos.width = Math.round(this.image.width * (this.stageHeight / this.image.height));
+            this.imgPos.x = Math.round((this.stageWidth - this.imgPos.width) / 2);
+        } else {
+            this.imgPos.height = Math.round(this.image.height * (this.stageWidth / this.image.width));
+            this.imgPos.y = Math.round((this.stageHeight - this.imgPos.height) / 2);
+        }
+
+        this.ctx.drawImage(this.image, 0, 0, this.image.width, this.image.height, this.imgPos.x, this.imgPos.y, this.imgPos.width, this.imgPos.height);
+
+        this.tmpCtx.drawImage(this.image, 0, 0, this.image.width, this.image.height, this.imgPos.x, this.imgPos.y, this.imgPos.width, this.imgPos.height);
+
+        this.imgData = this.tmpCtx.getImageData(0, 0, this.stageWidth, this.stageHeight);
+
+        this.drawDots();
+    }
+
+    drawDots() {
+        this.dots = [];
+
+        this.columns = Math.ceil(this.stageWidth / this.pixelSize);
+        this.rows = Math.ceil(this.stageHeight / this.pixelSize);
+
+        for (let i = 0; i < this.rows; i++) {
+            const y = (i + 0.5) * this.pixelSize;
+            const pixelY = Math.max(Math.min(y, this.stageHeight), 0);
+
+            for (let j = 0; j < this.columns; j++) {
+                const x = (j + 0.5) * this.pixelSize;
+                const pixelX = Math.max(Math.min(x, this.stageWidth), 0);
+
+                const pixelIndex = (pixelX + pixelY * this.stageWidth) * 4;
+                const red = this.imgData.data[pixelIndex + 0];
+                const green = this.imgData.data[pixelIndex + 1];
+                const blue = this.imgData.data[pixelIndex + 2];
+
+                const dot = new Dot(x, y, this.radius, this.pixelSize, red, green, blue);
+
+                this.dots.push(dot);
+            }
+        }
+    }
+
+    animate() {
+        window.requestAnimationFrame(this.animate.bind(this));
+
+        this.ripple.animate(this.ctx);
+
+        for (let i = 0; i < this.dots.length; i++) {
+            const dot = this.dots[i];
+
+            if (collide(dot.x, dot.y, this.ripple.x, this.ripple.y, this.ripple.radius)) {
+                dot.animate(this.ctx);
+            }
+        }
+    }
+
+    onClick(e) {
+        this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+
+        for (let i = 0; i < this.dots.length; i++) {
+            this.dots[i].reset();
+        }
+
+        this.ctx.drawImage(this.image, 0, 0, this.image.width, this.image.height, this.imgPos.x, this.imgPos.y, this.imgPos.width, this.imgPos.height);
+
+        this.ripple.start(e.offsetX, e.offsetY);
+    }
 }
 
 window.onload = () => {
-  new App();
+    new App();
 };
--- canvas/ripple.js
+++ canvas/ripple.js
@@ -1,37 +1,42 @@
 import { distance } from "./utils.js";
 
 export class Ripple {
-  constructor() {
-    this.x = 0;
-    this.y = 0;
-    this.radius = 0;
-    this.maxRadius = 0;
-    this.speed = 10;
-  }
-
-  resize(stageWidth, stageHeight) {
-    this.stageWidth = stageWidth;
-    this.stageHeight = stageHeight;
-  }
-
-  start(x, y) {
-    this.x = x;
-    this.y = y;
-    thjis.radius = 0;
-    this.maxRadius = this.getMax(x, y);
-  }
-
-  animate() {
-    if (this.radius < this.maxRadius) {
-      this.radius += this.speed;
+    constructor() {
+        this.x = 0;
+        this.y = 0;
+        this.radius = 0;
+        this.maxRadius = 0;
+        this.speed = 15;
     }
-  }
 
-  getMax(x, y) {
-    const c1 = distance(0, 0, x, y);
-    const c2 = distance(this.stageWidth, 0, x, y);
-    const c3 = distance(0, this.stageHeight, x, y);
-    const c4 = distance(this.stageWidth, this.stageHeight, x, y);
-    return Math.max(c1, c2, c3, c4);
-  }
+    resize(stageWidth, stageHeight) {
+        this.stageWidth = stageWidth;
+        this.stageHeight = stageHeight;
+    }
+
+    start(x, y) {
+        this.x = x;
+        this.y = y;
+        this.radius = 0;
+        this.maxRadius = this.getMax(x, y);
+    }
+
+    animate(ctx) {
+        if (this.radius < this.maxRadius) {
+            this.radius += this.speed;
+        }
+
+        ctx.beginPath();
+        ctx.fillStyle = "#00ff00";
+        ctx.arc(this.x, this.y, this.radius, Math.PI * 2, false);
+        ctx.fill();
+    }
+
+    getMax(x, y) {
+        const c1 = distance(0, 0, x, y);
+        const c2 = distance(this.stageWidth, 0, x, y);
+        const c3 = distance(0, this.stageHeight, x, y);
+        const c4 = distance(this.stageWidth, this.stageHeight, x, y);
+        return Math.max(c1, c2, c3, c4);
+    }
 }

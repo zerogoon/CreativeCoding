f41d204bbc2102805d0d6a217ac6373b98dc6902
zerogoon <kckzero@gmail.com>
2022-05-21 12:41:20 +0900

    Kinetic Typography #2_settings

---
+++ kinetic7/app.js
@@ -0,0 +1,14 @@
+class App {
+    constructor() {
+        WebFont.load({
+            google: {
+                families: ["Hind:700"],
+            },
+            fontactive: () => {},
+        });
+    }
+}
+
+window.onload = () => {
+    new App();
+};

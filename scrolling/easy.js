23b89ddd44b5c248bf71f42f5236ba900c27c248
zerogoon <kckzero@gmail.com>
2022-06-21 13:25:24 +0900

    easy scrolling variable type modified

--- scrolling/easy.js
+++ scrolling/easy.js
@@ -1,18 +1,18 @@
 const body = document.body,
-      jsScroll = document.getElementsByClassName('js-scroll')[0],
-      height = jsScroll.getBoundingClientRect().height - 1,
-      speed = 0.05
+    jsScroll = document.getElementsByClassName("js-scroll")[0],
+    height = jsScroll.getBoundingClientRect().height - 1,
+    speed = 0.05;
 
-var offset = 0
+let offset = 0;
 
-body.style.height = Math.floor(height) + "px"
+body.style.height = Math.floor(height) + "px";
 
 function smoothScroll() {
-    offset += (window.pageYOffset - offset) * speed
-    
-    var scroll = "translateY(-" + offset + "px) translateZ(0)"
-    jsScroll.style.transform = scroll
-    
-    raf = requestAnimationFrame(smoothScroll)
+    offset += (window.pageYOffset - offset) * speed;
+
+    const scroll = "translateY(-" + offset + "px) translateZ(0)";
+    jsScroll.style.transform = scroll;
+
+    raf = requestAnimationFrame(smoothScroll);
 }
-smoothScroll()
+smoothScroll();
